884a62e452470bb59ef9133c331c2cb3f68ae7ca
Sharath Chandra Pyarasani <sharathchandra.pyarasani@sarasanalytics.com>
2021-11-12 18:02:06 +0530

    Master Password scroll issue fixed

--- lib/src/auth/auth_view.dart
+++ lib/src/auth/auth_view.dart
@@ -46,160 +46,162 @@ class _AuthViewState extends State<AuthView> {
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
       child: SafeArea(
-        child: Column(
-          children: [
-            isMasterPass
-                ? Expanded(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Expanded(
-                          child: Image.asset(
-                            'assets/images/logo-android.png',
-                            scale: 10,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              isMasterPass
+                  ? Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: [
+                          Expanded(
+                            child: Image.asset(
+                              'assets/images/logo-android.png',
+                              scale: 10,
+                            ),
                           ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 16.0),
-                          child: Text(
-                            'Card Box',
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 16.0),
+                            child: Text(
+                              'Card Box',
+                              style: TextStyle(
+                                fontSize: 34.0,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  : Form(
+                      key: formKey,
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 30.0),
+                          const Text(
+                            'Setup Master Password',
                             style: TextStyle(
-                              fontSize: 34.0,
-                              color: Colors.grey,
+                              color: CupertinoColors.inactiveGray,
+                              fontSize: 24.0,
                               fontWeight: FontWeight.bold,
                             ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(vertical: 24.0),
+                            child: Icon(
+                              Icons.security,
+                              color: CupertinoColors.inactiveGray,
+                              size: 80.0,
+                            ),
+                          ),
+                          const Text(
+                            'Create a strong Master Password that will protect all your data stored in Card Box.',
                             textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: CupertinoColors.inactiveGray,
+                              fontSize: 16.0,
+                            ),
                           ),
-                        )
-                      ],
-                    ),
-                  )
-                : Form(
-                    key: formKey,
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 30.0),
-                        const Text(
-                          'Setup Master Password',
-                          style: TextStyle(
-                            color: CupertinoColors.inactiveGray,
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(vertical: 24.0),
-                          child: Icon(
-                            Icons.security,
-                            color: CupertinoColors.inactiveGray,
-                            size: 80.0,
-                          ),
-                        ),
-                        const Text(
-                          'Create a strong Master Password that will protect all your data stored in Card Box.',
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            color: CupertinoColors.inactiveGray,
-                            fontSize: 16.0,
-                          ),
-                        ),
-                        const SizedBox(height: 30.0),
-                        SizedBox(
-                          height: 130.0,
-                          child: Column(
-                            children: [
-                              const Text(
-                                'Password must contain:',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: CupertinoColors.inactiveGray,
-                                  fontSize: 16.0,
+                          const SizedBox(height: 30.0),
+                          SizedBox(
+                            height: 130.0,
+                            child: Column(
+                              children: [
+                                const Text(
+                                  'Password must contain:',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: CupertinoColors.inactiveGray,
+                                    fontSize: 16.0,
+                                  ),
                                 ),
-                              ),
-                              const SizedBox(height: 10.0),
-                              GridView.count(
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 24.0),
-                                shrinkWrap: true,
-                                crossAxisCount: 2,
-                                childAspectRatio: 5.0,
-                                children: const [
-                                  PasswordCriteria(label: 'One uppercase'),
-                                  PasswordCriteria(label: 'One lowercase'),
-                                  PasswordCriteria(label: 'One number'),
-                                  PasswordCriteria(
-                                      label: 'One special character'),
-                                ],
-                              ),
-                              const SizedBox(height: 5.0),
-                              const PasswordCriteria(
-                                label: 'Must be atleast 8 characters',
-                                center: true,
+                                const SizedBox(height: 10.0),
+                                GridView.count(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 24.0),
+                                  shrinkWrap: true,
+                                  crossAxisCount: 2,
+                                  childAspectRatio: 5.0,
+                                  physics: const NeverScrollableScrollPhysics(),
+                                  children: const [
+                                    PasswordCriteria(label: 'One uppercase'),
+                                    PasswordCriteria(label: 'One lowercase'),
+                                    PasswordCriteria(label: 'One number'),
+                                    PasswordCriteria(
+                                        label: 'One special character'),
+                                  ],
+                                ),
+                                const SizedBox(height: 5.0),
+                                const PasswordCriteria(
+                                  label: 'Must be atleast 8 characters',
+                                  center: true,
+                                ),
+                              ],
+                            ),
+                          ),
+                          const SizedBox(height: 20.0),
+                          CupertinoFormSection(
+                            children: [
+                              CupertinoFormRow(
+                                child: CupertinoTextFormFieldRow(
+                                  obscureText: true,
+                                  padding: EdgeInsets.zero,
+                                  controller: _masterPassController,
+                                  placeholder: 'Master Passsord',
+                                  keyboardType: TextInputType.text,
+                                  prefix: const Icon(Icons.lock_outline),
+                                  autovalidateMode:
+                                      AutovalidateMode.onUserInteraction,
+                                  autocorrect: false,
+                                  validator: (String? value) {
+                                    String pattern =
+                                        r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
+                                    RegExp regex = RegExp(pattern);
+                                    if (value!.isEmpty) {
+                                      return 'Please enter Master Password';
+                                    } else {
+                                      if (!regex.hasMatch(value)) {
+                                        return 'Enter valid Master Password';
+                                      } else {
+                                        return null;
+                                      }
+                                    }
+                                  },
+                                ),
                               ),
                             ],
                           ),
-                        ),
-                        const SizedBox(height: 20.0),
-                        CupertinoFormSection(
-                          children: [
-                            CupertinoFormRow(
-                              child: CupertinoTextFormFieldRow(
-                                obscureText: true,
-                                autofocus: true,
-                                padding: EdgeInsets.zero,
-                                controller: _masterPassController,
-                                placeholder: 'Master Passsord',
-                                keyboardType: TextInputType.text,
-                                prefix: const Icon(Icons.lock_outline),
-                                autovalidateMode:
-                                    AutovalidateMode.onUserInteraction,
-                                autocorrect: false,
-                                validator: (String? value) {
-                                  String pattern =
-                                      r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#\$&*~]).{8,}$';
-                                  RegExp regex = RegExp(pattern);
-                                  if (value!.isEmpty) {
-                                    return 'Please enter Master Password';
-                                  } else {
-                                    if (!regex.hasMatch(value)) {
-                                      return 'Enter valid Master Password';
-                                    } else {
-                                      return null;
-                                    }
-                                  }
-                                },
+                          const SizedBox(height: 20.0),
+                          CupertinoButton.filled(
+                            padding: const EdgeInsets.symmetric(
+                              horizontal: 28.0,
+                            ),
+                            child: const Text(
+                              'Save',
+                              style: TextStyle(
+                                color: CupertinoColors.white,
+                                fontSize: 14.0,
                               ),
                             ),
-                          ],
-                        ),
-                        const SizedBox(height: 20.0),
-                        CupertinoButton.filled(
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 28.0,
+                            onPressed: () async {
+                              if (formKey.currentState!.validate()) {
+                                await AuthService.instance
+                                    .setMasterPass(_masterPassController.text);
+                                Navigator.pushReplacementNamed(
+                                  context,
+                                  GroupListView.routeName,
+                                );
+                              }
+                            },
                           ),
-                          child: const Text(
-                            'Save',
-                            style: TextStyle(
-                              color: CupertinoColors.white,
-                              fontSize: 14.0,
-                            ),
-                          ),
-                          onPressed: () async {
-                            if (formKey.currentState!.validate()) {
-                              await AuthService.instance
-                                  .setMasterPass(_masterPassController.text);
-                              Navigator.pushReplacementNamed(
-                                context,
-                                GroupListView.routeName,
-                              );
-                            }
-                          },
-                        ),
-                      ],
-                    ),
-                  )
-          ],
+                        ],
+                      ),
+                    )
+            ],
+          ),
         ),
       ),
     );
